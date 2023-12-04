819f2840a05d368b37e2723f34ff29d5a1b6988a
m-tkd <m-takeda731@outlook.jp>
2023-12-04 23:59:01 +0900

    customers

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -43,6 +43,12 @@ const showingNavigationDropdown = ref(false);
                                 >
                                     商品管理
                                 </NavLink>
+                                <NavLink
+                                    :href="route('customers.index')"
+                                    :active="route().current('customers.index')"
+                                >
+                                    顧客管理
+                                </NavLink>
                             </div>
                         </div>
 
@@ -155,6 +161,12 @@ const showingNavigationDropdown = ref(false);
                             :active="route().current('items.index')"
                         >
                             商品管理
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink
+                            :href="route('customers.index')"
+                            :active="route().current('customers.index')"
+                        >
+                            顧客管理
                         </ResponsiveNavLink>
                     </div>
 
